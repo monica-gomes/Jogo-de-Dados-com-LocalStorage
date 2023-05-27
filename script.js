f3fabd74b457380af93ce624217b90a06740476b
Monica Gomes <gomescsmonica@gmail.com>
2023-05-27 00:13:17 -0300

    Implementação LocalStorage para registrar partida

--- script.js
+++ script.js
@@ -4,47 +4,54 @@ const btReiniciar = document.getElementById('btReiniciar');
 const resultado = document.getElementById('resultado');
 const numRodada = document.getElementById('numeroRodada');
 
-let Jogador1 = 0;
-let Jogador2 = 0;
-let jogadaAtual = 1;
+let jogadorAtual = 1;
+let rodadaAtual = 1;
 let rodadasJogadas = 0;
-let numeroRodada = 0;
+let jogador1Pontos = 0;
+let jogador2Pontos = 0;
 
 function salvarJogo() {
-    localStorage.setItem('Jogador1', Jogador1);
-    localStorage.setItem('Jogador2', Jogador2);
-    localStorage.setItem('jogadaAtual', jogadaAtual);
-    localStorage.setItem('rodadasJogadas', rodadasJogadas);
-    localStorage.setItem('numeroRodada', numeroRodada);
-  }
+    const jogo = {
+        jogadorAtual: jogadorAtual,
+        rodadaAtual: rodadaAtual,
+        rodadasJogadas: rodadasJogadas,
+        jogador1Pontos: jogador1Pontos,
+        jogador2Pontos: jogador2Pontos
+    };
 
-  function carregarJogo() {
-    Jogador1 = parseInt(localStorage.getItem('Jogador1')) || 0;
-    Jogador2 = parseInt(localStorage.getItem('Jogador2')) || 0;
-    jogadaAtual = parseInt(localStorage.getItem('jogadaAtual')) || 1;
-    rodadasJogadas = parseInt(localStorage.getItem('rodadasJogadas')) || 0;
-    numeroRodada = parseInt(localStorage.getItem('numeroRodada')) || 0;
-    
-    numRodada.innerHTML = rodadasJogadas;
+    localStorage.setItem('jogoDados', JSON.stringify(jogo));
+}
+
+function carregarJogo() {
+    const jogo = localStorage.getItem('jogoDados');
+    if (jogo) {
+        const dados = JSON.parse(jogo);
+        jogadorAtual = dados.jogadorAtual;
+        rodadaAtual = dados.rodadaAtual;
+        rodadasJogadas = dados.rodadasJogadas;
+        jogador1Pontos = dados.jogador1Pontos;
+        jogador2Pontos = dados.jogador2Pontos;
+        numRodada.innerHTML = rodadaAtual;
+
+        exibirResultados();
+    }
 }
 
 function rodarDado() {
     const sorteio = Math.floor(Math.random() * 6) + 1;
-    resultado.innerText = `Jogador ${jogadaAtual}: ${sorteio}`;
+    resultado.innerText = `Jogador ${jogadorAtual}: ${sorteio}`;
 
-    if (jogadaAtual === 1) {
-        Jogador1 += sorteio;
-        jogadaAtual = 2;
-        
+    if (jogadorAtual === 1) {
+        jogador1Pontos += sorteio;
+        jogadorAtual = 2;
         btJogador1.disabled = true;
         btJogador2.disabled = false;
-    }
-    else {
-        Jogador2 += sorteio;
-        jogadaAtual = 1;
+    } else {
+        jogador2Pontos += sorteio;
+        jogadorAtual = 1;
+        rodadaAtual++;
         rodadasJogadas++;
-        numRodada.innerHTML = rodadasJogadas;
-
+        numRodada.innerHTML = rodadaAtual;
         btJogador1.disabled = false;
         btJogador2.disabled = true;
     }
@@ -52,32 +59,48 @@ function rodarDado() {
     salvarJogo();
 
     if (rodadasJogadas === 10) {
-        if (Jogador1 > Jogador2) {
-            resultado.innerText = "\nJogador 1: Você Venceu!";
-        }
-        else if (Jogador2 > Jogador1) {
-            resultado.innerText = "\nJogador 2: Você Venceu!";
-        }
-        else {
-            resultado.innerText = "\nEmpate!";
-        }
-
+        exibirResultados();
         btJogador1.disabled = true;
         btJogador2.disabled = true;
     }
-    salvarJogo();
 }
 
-btJogador1.onclick = rodarDado;
-btJogador2.onclick = rodarDado;
-btReiniciar.onclick = function () {
-    Jogador1 = 0;
-    Jogador2 = 0;
-    jogadaAtual = 1;
+function reiniciarJogo() {
+    jogadorAtual = 1;
+    rodadaAtual = 1;
     rodadasJogadas = 0;
+    jogador1Pontos = 0;
+    jogador2Pontos = 0;
     numRodada.innerHTML = 0;
     resultado.innerText = "";
     btJogador1.disabled = false;
     btJogador2.disabled = false;
+
+    salvarJogo();
 }
+
+function exibirResultados() {
+    const resultadoJogador1 = `Jogador 1: ${jogador1Pontos}`;
+    const resultadoJogador2 = `Jogador 2: ${jogador2Pontos}`;
+
+    resultado.innerHTML = `${resultadoJogador1}<br>${resultadoJogador2}`;
+
+    if (rodadasJogadas === 10) {
+        let vencedor = "";
+        if (jogador1Pontos > jogador2Pontos) {
+            vencedor = "Jogador 1 venceu!";
+        } else if (jogador2Pontos > jogador1Pontos) {
+            vencedor = "Jogador 2 venceu!";
+        } else {
+            vencedor = "Empate!";
+        }
+
+        resultado.innerHTML += `<br>${vencedor}`;
+    }
+}
+
+btJogador1.addEventListener('click', rodarDado);
+btJogador2.addEventListener('click', rodarDado);
+btReiniciar.addEventListener('click', reiniciarJogo);
+
 carregarJogo();
